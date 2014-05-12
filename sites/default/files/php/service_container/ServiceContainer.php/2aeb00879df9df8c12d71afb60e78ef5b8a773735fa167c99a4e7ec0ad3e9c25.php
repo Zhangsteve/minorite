348aacb541782d2171d222bc21fae29014c21f75
zhangsteve <seakiro@msn.com>
2014-05-12 18:25:31 +0200

    test

---
+++ sites/default/files/php/service_container/ServiceContainer.php/2aeb00879df9df8c12d71afb60e78ef5b8a773735fa167c99a4e7ec0ad3e9c25.php
@@ -0,0 +1,42 @@
+<?php
+
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use Symfony\Component\DependencyInjection\Container;
+use Symfony\Component\DependencyInjection\Exception\InactiveScopeException;
+use Symfony\Component\DependencyInjection\Exception\InvalidArgumentException;
+use Symfony\Component\DependencyInjection\Exception\LogicException;
+use Symfony\Component\DependencyInjection\Exception\RuntimeException;
+use Symfony\Component\DependencyInjection\ParameterBag\ParameterBag;
+
+/**
+ * ServiceContainer
+ *
+ * This class has been auto-generated
+ * by the Symfony Dependency Injection Component.
+ */
+class ServiceContainer extends Container
+{
+    /**
+     * Constructor.
+     */
+    public function __construct()
+    {
+        parent::__construct();
+        $this->methodMap = array(
+            'user.entity.storage' => 'getUser_Entity_StorageService',
+        );
+    }
+
+    /**
+     * Gets the 'user.entity.storage' service.
+     *
+     * This service is shared.
+     * This method always returns the same instance of the service.
+     *
+     * @return Mini\User\Entity\UserStorageController A Mini\User\Entity\UserStorageController instance.
+     */
+    protected function getUser_Entity_StorageService()
+    {
+        return $this->services['user.entity.storage'] = call_user_func(array('Drupal\\Core\\DependencyInjection\\EntityControllerFactory', 'get'), 'user');
+    }
+}
